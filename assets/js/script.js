65f1322a22935f22aa6d2424fab14bfc6c275ff1
HacAtac <jhack00@icloud.com>
2021-09-01 17:19:40 -0500

    added some pseudo code and used dom manipulation to create home screen elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,27 @@
-var timerEl = document.getElementById('countdown');
-var mainEl = document.getElementById('main');
-var startBtn = document.getElementById('start');
+var main = document.querySelector("#main");
+//created function to contain main mage content
+var start = function() { 
+    var container = document.createElement("div");//making a div element
+    container.className= "first-container"//naming the div first-container
+    main.appendChild(container);//appending in the main section
 
-// timer that counts down from 70
-function timer(){
-    var sec = 70;
-    var timer = setInterval(function(){
-        document.getElementById('timerDisplay').innerHTML='00:'+sec;
-        sec--;
-        if (sec < 0) {
-            clearInterval(timer);
-        }
-    }, 1000);
+//created h1 element 
+    var firstHeader = document.createElement("h1");
+    firstHeader.className = "first-title";
+    firstHeader.textContent = "Coding Quiz Challenge";
+    container.appendChild(firstHeader);
+//created p ele
+    var firstParagraph = document.createElement("p");
+    firstParagraph.className = "first-p";
+    firstParagraph.textContent = "Try to answer the following questions within the time limit" + "Keep in mind that answering wrong will take 10 seconds away from timer!"
+    container.appendChild(firstParagraph);
+//created a button
+    var startBtn= document.createElement("button");
+    startBtn.className = "home-btn";
+    startBtn.textContent = "Start Quiz";
+    container.appendChild(startBtn);
 }
-timer();
-
-
-
-
+//calling the start function
+start();
 
 
