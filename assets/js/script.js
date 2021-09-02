af39be73e72fb6189aa62564be45b7fbd76ae492
HacAtac <jhack00@icloud.com>
2021-09-01 21:26:17 -0500

    added 4 questions and answers and if answer wrong or correct it goes to next question, also reduces time by 10 if answer wrong

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,16 @@
 var main = document.querySelector("#main");
 var timer = document.querySelector("#timer");//targets the timer id in html with dom manupulation
-var time = 70;
-//timer with 70 seconds
+var time = 30;
+//timer with 30 seconds
 var timer = function() {
+    
     var timeInterval = setInterval(() => {
         if (time > 1) {
             timer.textContent = time;
             time --;
             console.log(time);
         }else {
-            timer.textContent = "You're done son!";
+            timer.textContent = "";
             clearInterval(timeInterval);
         }
     },1000);
@@ -23,6 +24,7 @@ var question1 = function() {
     main.appendChild(question1Div);
     //question
     var question1 = document.createElement("h1");
+    question1.className = "question";
     question1.textContent = "Commonly Used data types DO NOT include:";
     question1Div.appendChild(question1);
 
@@ -74,6 +76,7 @@ var question1 = function() {
     }
 }
 
+
 //question 2
 var question2 = function () {
     var question2Div = document.createElement("div");
@@ -81,26 +84,58 @@ var question2 = function () {
     main.appendChild(question2Div);
     //question
     var question2 = document.createElement("h1");
-    question2.textContent = "Placeholder till i figure out questions";
+    question2.className = "question";
+    question2.textContent = "Inside which HTML element do we put the JavaScript?";
     question2Div.appendChild(question2);
 
     //answers
     var answer1 = document.createElement("button");
-    answer1.textContent = "placehold";
+    answer1.setAttribute("id", "wrong");
+    answer1.className = "answer-1";
+    answer1.textContent = "<js>";
     question2Div.appendChild(answer1);
+    answer1.addEventListener("click", event => {
+        wrongAns();
+    })
 
     var answer2 = document.createElement("button");
-    answer2.textContent = "placehold";
+    answer2.setAttribute("id", "wrong");
+    answer2.className = "answer-2";
+    answer2.textContent = "<javascript>";
     question2Div.appendChild(answer2);
+    answer2.addEventListener("click", event => {
+        wrongAns();
+    })
 
     var answer3 = document.createElement("button");
-    answer3.textContent = "placehold";
+    answer3.setAttribute("id", "correct");
+    answer3.textContent = "<script>";
     question2Div.appendChild(answer3);
+    answer3.addEventListener("click", event => {
+        question2Div.remove();
+        question3();
+        console.log("Correct!");
+    })
+
 
     var answer4 = document.createElement("button");
-    answer4.textContent = "placehold";
+    answer4.setAttribute("id", "wrong");
+    answer4.className = "answer-4";
+    answer4.textContent = "<scripting>";
     question2Div.appendChild(answer4);
+    answer4.addEventListener("click", event => {
+        wrongAns();
+    })
+
+    var wrongAns = function() {
+        time = time - 10;
+        question1Div.remove();
+        question3();
+        console.log("Wrong answer!");
+    }
 }
+
+
 
 //question 3
 var question3 = function () {
@@ -109,26 +144,58 @@ var question3 = function () {
     main.appendChild(question3Div);
     //question
     var question3 = document.createElement("h1");
-    question3.textContent = "Placeholder till i figure out questions";
+    question3.textContent = "How do you call a function named 'myFunction' ";
     question3Div.appendChild(question3);
 
     //answers
     var answer1 = document.createElement("button");
-    answer1.textContent = "placehold";
+    answer1.setAttribute("id", "correct");
+    answer1.className = "answer-1";
+    answer1.textContent = "myFunction()";
     question3Div.appendChild(answer1);
+    answer1.addEventListener("click", event => {
+        question3Div.remove();
+        question4();
+        console.log("Correct!");
+    })
 
     var answer2 = document.createElement("button");
-    answer2.textContent = "placehold";
+    answer2.setAttribute("id", "wrong");
+    answer2.className = "answer-2";
+    answer2.textContent = "call function myFunction()";
     question3Div.appendChild(answer2);
+    answer2.addEventListener("click", event => {
+        wrongAns();
+    })
+
 
     var answer3 = document.createElement("button");
-    answer3.textContent = "placehold";
+    answer3.setAttribute("id", "wrong");
+    answer3.className = "answer-3";
+    answer3.textContent = "call myFunction()";
     question3Div.appendChild(answer3);
+    answer3.addEventListener("click", event => {
+        wrongAns();
+    })
 
     var answer4 = document.createElement("button");
-    answer4.textContent = "placehold";
+    answer4.setAttribute("id", "wrong");
+    answer4.className = "answer-4";
+    answer4.textContent = "MyFunction()";
     question3Div.appendChild(answer4);
+    answer4.addEventListener("click", event => {
+        wrongAns();
+    })
+
+    var wrongAns = function() {
+        time = time - 10;
+        question1Div.remove();
+        question4();
+        console.log("Wrong answer!");
+    }
 }
+
+
 //question 4
 var question4 = function () {
     var question4Div = document.createElement("div");
@@ -136,26 +203,50 @@ var question4 = function () {
     main.appendChild(question4Div);
     //questions
     var question4 = document.createElement("h1");
-    question4.textContent = "placehodler till i figure out questions";
+    question4.textContent = "How do you round the number 7.25, to the nearest integer?";
     question4Div.appendChild(question4);
 
     //answer
     var answer1 = document.createElement("button");
-    answer1.textContent = "placehold";
+    answer1.setAttribute("id", "wrong");
+    answer1.className = "answer-1";
+    answer1.textContent = "rnd(7.25)";
     question4Div.appendChild(answer1);
+    answer1.addEventListener("click", event => {
+        wrongAns();
+    })
 
     var answer2 = document.createElement("button");
-    answer2.textContent = "placehold";
+    answer2.setAttribute("id", "wrong");
+    answer2.className = "answer-2";
+    answer2.textContent = "Math.rnd(7.25)";
     question4Div.appendChild(answer2);
+    answer2.addEventListener("click", event => {
+        wrongAns();
+    })
 
     var answer3 = document.createElement("button");
-    answer3.textContent = "placehold";
+    answer3.setAttribute("id", "wrong");
+    answer3.className = "answer-3";
+    answer3.textContent = "round(7.25)";
     question4Div.appendChild(answer3);
+    answer3.addEventListener("click", event => {
+        wrongAns();
+    })
 
-    var answer4 = document.createElement("h1");
-    answer4.textContent = "placehold";
+    var answer4 = document.createElement("button");
+    answer4.setAttribute("id", "correct");
+    answer4.className = "answer-4";
+    answer4.textContent = "Math.round(7.25)";
     question4Div.appendChild(answer4);
+    answer4.addEventListener("click", event => {
+        question4Div.remove();
+        question5();
+        console.log("Correct!");
+    })
 }
+
+
 //question 5
 var question5 = function () {
     var question5Div = document.createElement("div");
@@ -164,25 +255,46 @@ var question5 = function () {
     
     //questions
     var question5 = document.createElement("h1");
-    question5.textContent = "placeholder till i get questions";
+    question5.textContent = "How do you find the number with the highest value of x and y?";
     question5Div.appendChild(question5);
 
     //answers
     var answer1 = document.createElement("button");
-    answer1.textContent = "placehold";
+    answer1.setAttribute("id", "correct");
+    answer1.className = "answer-1";
+    answer1.textContent = "Math.max(x, y)";
     question5Div.appendChild(answer1);
+    answer1.addEventListener("click", event => {
+        question5Div.remove();
+        console.log("Correct!");
+    })
 
     var answer2 = document.createElement("button");
-    answer2.textContent = "placehold";
+    answer2.setAttribute("id", "wrong");
+    answer2.className = "answer-2";
+    answer2.textContent = "top(x,y)";
     question5Div.appendChild(answer2);
+    answer2.addEventListener("click", event => {
+        wrongAns();
+    })
 
     var answer3 = document.createElement("button");
-    answer3.textContent = "placehold";
+    answer3.setAttribute("id", "wrong");
+    answer3.className = "answer-3";
+    answer3.textContent = "Math.ceil(x,y)";
     question5Div.appendChild(answer3);
+    answer3.addEventListener("click", event => {
+        wrongAns();
+    })
 
     var answer4 = document.createElement("button");
-    answer4.textContent = "placehold";
+    answer4.setAttribute("id", "wrong");
+    answer4.className = "answer-4";
+    answer4.textContent = "ceil(x,y)";
     question5Div.appendChild(answer4);
+    answer4.addEventListener("click", event => {
+        wrongAns();
+    })
 
 }
 
@@ -192,22 +304,26 @@ var start = function() {
     container.className= "first-container"//naming the div first-container
     main.appendChild(container);//appending in div container
 
-//created h1 element 
+
+    //created h1 element 
     var firstHeader = document.createElement("h1");
     firstHeader.className = "first-title";
     firstHeader.textContent = "Coding Quiz Challenge";
     container.appendChild(firstHeader);
-//created p ele
+
+    //created p ele
     var firstParagraph = document.createElement("p");
     firstParagraph.className = "first-p";
     firstParagraph.textContent = "Try to answer the following questions within the time limit" + "Keep in mind that answering wrong will take 10 seconds away from timer!"
     container.appendChild(firstParagraph);
-//created a button
+
+    //created a button
     var startBtn= document.createElement("button");
     startBtn.className = "home-btn";
     startBtn.textContent = "Start Quiz";
     container.appendChild(startBtn);
-//created event lister for start quiz button and removes the previos manipulations from page
+
+    //created event lister for start quiz button and removes the previos manipulations from page
     startBtn.addEventListener("click", event => {
         timer();//calls timer function and starts it
         startBtn.remove();//removes from home page on click
